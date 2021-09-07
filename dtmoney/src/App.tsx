6b561d3ab7e485770276edf26940d059fbfe7246
jose-rgb <josericardosaraiva7@gmail.com>
2021-09-07 10:57:40 -0300

    Application structure

---
+++ dtmoney/src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyle } from "./styles/global";
+
+
+export function App() {
+  return (
+    <div className="App">
+     <h1>hello dtmoney</h1>
+     <GlobalStyle />
+    </div>
+  );
+}
+ 
+  
